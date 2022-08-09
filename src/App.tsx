0b5c6f74afdb96d735a7c36d8ff1cdd7c5d531f3
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-10 01:24:16 +0300

    feat: useEffect and async-await func fetchUsers

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import Card, {CardVariant} from './components/Card'
 import UserList from './components/UserList'
 import { IUser } from './types/types'
@@ -6,28 +7,21 @@ import { IUser } from './types/types'
 
 function App() {
 
-  const users: IUser[] = [
-    {
-      id: 1,
-      name: "Leanne Graham",
-      email: "Sincere@april.biz",
-      address: {
-      street: "Kulas Light",
-      city: "Gwenborough",
-      zipcode: "92998-3874",
-      }
-    },
-    {
-      id: 2,
-      name: "Ervin Howell",
-      email: "Shanna@melissa.tv",
-      address: {
-        street: "Victor Plains",
-        city: "Wisokyburgh",
-        zipcode: "90566-7771",
-      },
-    },
-  ]
+  const [users, setUsers] = useState<IUser[]>([])
+
+  useEffect( () => {
+    fetchUsers()
+  }, [])
+
+  async function fetchUsers() {
+    try {
+      const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+      setUsers(response.data)
+    } catch (error) {
+      console.log('error', error);
+
+    }
+  }
 
   return (
     <div>
