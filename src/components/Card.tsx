3de47abf093161d9fbe657c9d6e2a1cfad28d7e1
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-10 00:36:48 +0300

    feat: hand over variant styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,13 @@
 import React, {FC} from 'react'
+
+export enum CardVariant {
+    outlined = 'outlined',
+    primary = 'primary'
+}
 interface CardProps {
     width?: string;
     height?: string;
+    variant: CardVariant;
     children?: React.ReactChild | React.ReactNode
 }
 
@@ -9,10 +15,18 @@ const Card: FC<CardProps> =
     ({
         width,
         height,
-        children
+        children,
+        variant,
     }) => {
     return (
-        <div style={{width, height, border: '1px solid gray'}}>
+        <div style={{width, height,
+                    border: variant === CardVariant.outlined
+                      ? '1px solid gray'
+                      : 'none',
+                    background: variant === CardVariant.primary
+                      ? 'lightgray'
+                      : ''
+                    }}>
             {children}
         </div>
     )
